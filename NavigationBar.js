a440be686c33b33bc61df8ff8420d48fc01dd1d7
VajaOm <121480849+VajaOm@users.noreply.github.com>
2023-01-26 18:59:08 +0530

    Update NavigationBar.js

--- NavigationBar.js
+++ NavigationBar.js
@@ -1,6 +1,6 @@
 const toggleButton = document.getElementsByClassName('toggle-btn')[0];
 const navbar = document.getElementsByClassName("item")[0];
-const container_two = document.getElementsByClassName("container-two")[0]; 
+const container_two = document.getElementsByClassName("first-container-two")[0]; 
 let flag=false;
 
 toggleButton.addEventListener("click",() =>{
