05d408ca70eb9525eed36bc69196077af4c6e56b
VajaOm <121480849+VajaOm@users.noreply.github.com>
2023-02-01 16:38:26 +0530

    Update NavigationBar.js

--- NavigationBar.js
+++ NavigationBar.js
@@ -1,17 +1,24 @@
+
+
 const toggleButton = document.getElementsByClassName('toggle-btn')[0];
 const navbar = document.getElementsByClassName("item")[0];
 const container_two = document.getElementsByClassName("first-container-two")[0]; 
+let list_items = document.querySelectorAll(".list")[0];
 let flag=false;
+var root = document.querySelector(':root');
 
 toggleButton.addEventListener("click",() =>{
     // console.log("Hello")
+    // console.log(list_items);
     if(flag)
     {
+        root.style.setProperty('--top','clamp(33em,100vw + 0em,55em)');
     container_two.style.display = "inline-block";
     flag = false;
     }
     else{
+        root.style.setProperty('--top','clamp(25em,100vw + 0em,55em)');
         container_two.style.display = "none";
         flag = true;
     }
-})
+});
